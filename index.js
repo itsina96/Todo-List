d3e59611541e00b08a4a962fda9c97774b2707fe
Ina <53597131+itsina96@users.noreply.github.com>
2019-09-24 19:25:18 +0900

    added check icon

--- index.js
+++ index.js
@@ -1,175 +1,242 @@
-// Defind variables 
+// Declaration
 const date = document.querySelector('#date');
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clearBtn');
 const taskInput = document.querySelector('#task');
-const filter = document.querySelector('#filter');
+let index = 0;
+let i =0;
+let old;
 
-/**** Load all event listeners ****/
+// Load all event listeners
 loadEventListeners();
 
-function loadEventListeners() {
-    // DOM load event - for tasks from storage to be displayed
-    document.addEventListener('DOMContentLoaded', getTasks)
-    // Add-task event
+function loadEventListeners() {  
+    document.addEventListener('DOMContentLoaded', getTasks);
     form.addEventListener('submit', addTask);
-    // Remove-a-task event
     taskList.addEventListener('click', removeTask);
-    // Clear-all-task event
     clearBtn.addEventListener('click', clearAll);
 }
 
+function checkID(str){
+    let res;
+    res = str.replace(/[^0-9]/g,"");
+    return res;
+}
+
+
+
 // Get Tasks from Localstorage
 function getTasks() {
+    old = new Array();
     let tasks;
-    if(localStorage.getItem('tasks') === null) {
+    
+    let check = localStorage.getItem('tasks');
+    console.log(check);
+    if(check === null || typeof check == "undefined") {
         tasks = [];
+        old=[];
+        console.log(tasks);
     } else {
         tasks = JSON.parse(localStorage.getItem('tasks'));
+        
     }
     
+    
     tasks.forEach(function(task){
+        // console.log("get");
+        // console.log(task);
+        let v = checkID(task.id);
+        old[v]=true;
+        checkedIndex(old);
+        
+        const eachDiv = document.createElement('div');
+        eachDiv.className = 'each-div';
+        
+        // Add checkbox
+        
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        // checkbox.setAttribute('data-class',task.checked );
+        // checkbox.checked = task.checked;   
+        
+        
+        
+        checkbox.setAttribute('id', task.id);
+        
+        eachDiv.appendChild(checkbox);
+        
         // Create li element
         const li = document.createElement('label');
         // Add class
         li.className = 'collection-item';
-        li.setAttribute('for', 'connect');
-        
-        // Add checkbox
-        const checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.id = 'connect';
-        li.appendChild(checkbox);
+        li.setAttribute('for', task.id);
         
         // Add custom checkbox for styling purpose 
         const customCheckbox = document.createElement('span');
         customCheckbox.className = 'custom-checkbox';
         li.appendChild(customCheckbox);
-        // customCheckbox.innerHTML = '<i class="fas fa-check"></i>';
+        
         // Create text node and append to the li
-        li.appendChild(document.createTextNode(task));
+        li.appendChild(document.createTextNode(task.value));
         
         // Create new link element - for icon
         const link = document.createElement('a');
         link.className = 'delete-item';
         // Add icon HTML
         link.innerHTML = '<i class="fas fa-times"></i>';
+        
         // Append link to the li 
         li.appendChild(link);
         
-        // finally Append the li to the ul!
-        taskList.appendChild(li);
+        // Append the completed li to the each div
+        eachDiv.appendChild(li)
+        
+        // finally Append each div to collection!
+        taskList.appendChild(eachDiv); 
+        
+        
     });
+    
 }
+
 
 // Add-Task 
 function addTask(e) {
-    if(taskInput.value === '') {
-        alert('It seems you left your input empty. Please add a task and press the button.');
+    
+    if(taskInput.value == '') {
+        alert('Please add a task and press the button.');
     } else {
-        // Create li element
-        const li = document.createElement('label');
-        // Add class
-        li.className = 'collection-item';
-        li.setAttribute('for', 'connect');
         
-        // Add checkbox
+        index++;
+        
+        
+        
+        const eachDiv = document.createElement('div');
+        eachDiv.className = 'each-div';
+        
         const checkbox = document.createElement('input');
         checkbox.type = 'checkbox';
-        checkbox.id = 'connect';
-        li.appendChild(checkbox);
+        checkbox.setAttribute('id', 'connect'+index);
+        checkbox.setAttribute('name', 'contents');
+        eachDiv.appendChild(checkbox);
         
-        // Add custom checkbox for styling purpose 
+        const li = document.createElement('label');
+        li.className = 'collection-item';
+        li.setAttribute('for', 'connect'+index);
+        
         const customCheckbox = document.createElement('span');
         customCheckbox.className = 'custom-checkbox';
+        
+        
         li.appendChild(customCheckbox);
         
-        // customCheckbox.innerHTML = '<i class="fas fa-check"></i>';
-        // Create text node and append to the li
         li.appendChild(document.createTextNode(taskInput.value));
         
-        // Create new link element - for icon
         const link = document.createElement('a');
         link.className = 'delete-item';
-        
-        // Add icon HTML
         link.innerHTML = '<i class="fas fa-times"></i>';
-        
-        // Append link to the li 
         li.appendChild(link);
+        eachDiv.appendChild(li)
+        taskList.appendChild(eachDiv);
         
-        // finally Append the li to the ul!
-        taskList.appendChild(li);
+        // storeTaskInLocalStorage(taskInput.value);
         
-        // Store in local storage
-        storeTaskInLocalStorage(taskInput.value);
+        storeTaskInLocalStorage(checkbox,taskInput.value,checkbox.checked);
+        
     }
-    // once we do that, Clear the input so next task could be written
     taskInput.value = '';
     
     e.preventDefault();
 }
 
-// Store Task
-function storeTaskInLocalStorage(task) {
-    let tasks;
-    if(localStorage.getItem('tasks') === null) {
-        tasks = [];
-    } else {
-        tasks = JSON.parse(localStorage.getItem('tasks'));
+function checkedIndex(old){
+    for(var i=0; i<old.length; i++){
+        if(old[index]){
+            index++;
+        }else{
+            index=i;
+        }
     }
-    
-    tasks.push(task);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-// Remove a task - using event delegation
+
+
+// Store Task
+function storeTaskInLocalStorage(task,taskValue) {
+    let tasks;
+    //let ids;
+    //checkedBox(task.id);
+    
+    
+    if(localStorage.getItem('tasks') === null) {
+        tasks = [];
+        i=0;
+        //ids=[];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+        //ids=JSON.parse(localStorage.getItem('ids'));
+        
+    }
+    
+    
+    tasks.push({"id":task.id, "value":taskValue});
+    //ids.push(checkId);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    //localStorage.setItem('ids', JSON.stringify(ids));
+}
+
+// Remove a task
 function removeTask(e) {
     if(e.target.parentElement.classList.contains('delete-item')) {
-        e.target.parentElement.parentElement.remove();
         
+        e.target.parentElement.parentElement.remove();
+        console.log(e.target.parentElement.parentElement);
         removeTaskFromLocalStorage(e.target.parentElement.parentElement);
     }
 }
 
 // Remove a task - from LS as well
 function removeTaskFromLocalStorage(taskItem) {
+    // alert(taskItem);
     let tasks;
     if(localStorage.getItem('tasks') === null) {
         tasks = [];
+        //old=[];
     } else {
         tasks = JSON.parse(localStorage.getItem('tasks'));
+        
+        
     }
     
     tasks.forEach(function(task, index){
-        if(taskItem.textContent === task){
+        
+        console.log(taskItem.attributes[1].value);
+        if(taskItem.attributes[1].value === task.id){
+            console.log(taskItem);
             tasks.splice(index, 1);
         }
     });
+    
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // Clear-ALL-tasks
 function clearAll() {
-    // you can do taskList.innerHTML = '', but it's not faster than while version
     if(confirm('Are you sure you want to clear ALL? You cannot undo this action.')) {
         while(taskList.firstChild) {
             taskList.removeChild(taskList.firstChild);
-            // All clear from LS as well
             clearAllTaskFromLocalStorage();
         }
     } 
-    
 }
+
 // Clear All tasks from LS
 function clearAllTaskFromLocalStorage() {
     localStorage.clear();
 }
 
-/* Show Date */
+// Show Date
 let options = { weekday:'long', month:'short', day:'numeric'};
 let today = new Date();
 date.innerHTML = today.toLocaleDateString("en-US", options);
-
-
